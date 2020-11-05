e230cda91849d1247eab12a0d016fc367cfb9cc2
Robert Petersen <robert.petersen808@gmail.com>
2020-11-04 18:55:46 -0600

    yeah..

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@
   Look at each file to see what props need to be passed!
 */
 
+/*commit*/
+
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+
 // Import the dummyData
 import './App.css';
 
